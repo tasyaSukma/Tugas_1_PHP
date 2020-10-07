6ffb17010c3a4565c2444ce43b9b334761daae71
tasyaSukma <maftuhah.tasya@gmail.com>
2020-10-07 10:08:44 +0700

    Add files via upload

---
+++ tugassatu.php
@@ -0,0 +1,26 @@
+<?php
+	$ayamGoreng = 13000;
+	$ayamBakar = 15000;
+	$ayamSayur = 13000;
+	$tempeGoreng = 1000;
+	$tahuGoreng = 1000;
+	$soto = 3000;
+	$nasiPutih = 5000;
+	$esTeh = 4000;
+	$esTebu = 5000;
+
+	echo "<h1>Daftar Menu</h1>";
+	echo "<ul>
+			<li> Ayam Goreng  "."= ".$ayamGoreng."</li>
+			<li> Ayam Bakar   "."= ".$ayamBakar."</li>
+			<li> Ayam Sayur   "."= ".$ayamSayur."</li>
+			<li> Tempe Goreng "."= ".$tempeGoreng."</li>
+			<li> Tahu Goreng  "."= ".$tahuGoreng."</li>
+			<li> Soto         "."= ".$soto."</li>
+			<li> Nasi Putih   "."= ".$nasiPutih."</li>
+			<li> Es Teh       "."= ".$esTeh."</li>
+			<li> Es Tebu      "."= ".$esTebu."</li>
+		</ul>";
+
+	
+?>
